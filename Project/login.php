3e870f4be827324faeff3c6b6eeb8ebf9bde7f99
997962520 <997962520@qq.com>
2016-07-04 14:01:59 +0800

    登录修改

--- Project/login.php
+++ Project/login.php
@@ -10,30 +10,34 @@ $password = $_POST['password'];
 $role = $_POST['role'];
 $my_db=new database();
 
-if($username == "")
-{
+if($username == "") {
     echo"<script type='text/javascript'>alert('请填写用户名');location='login.html';</script>";
 }
-elseif($password == "")
-{
+elseif($password == "") {
     echo"<script type='text/javascript'>alert('请填写密码');location='login.html';</script>";
 }
-elseif ($role == "")
-{
+elseif ($role == "") {
     echo"<script type='text/javascript'>alert('请选择用户种类');location='login.html';</script>";
 }
-else
-{
+else {
     $table = $role;
     $field = $password;
     $keys = array('name'=>$username);
     $result = $my_db->select_data($table,$field,$keys);
-    if($result == $password)
-    {
+
+    if($role=="student") {
+        $id = $my_db->select_data($table, 'student_id', $keys);
+    }
+    else {
+        $id = $id = $my_db->select_data($table, 'employee_id', $keys);
+    }
+
+    if($result == $password) {
         $lifeTime = 120;
         session_set_cookie_params($lifeTime);
         session_start();
         $_SESSION["username"] = $username;
+        $_SESSION["id"] = $id;
         switch ($role)
         {
             case 'student';
@@ -47,8 +51,7 @@ else
                 break;
         }
     }
-    else
-    {
+    else {
         echo "<script type='text/javascript'>alert('用户名或密码错误');location='login.html';</script>";
     }
 }
