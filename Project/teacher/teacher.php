f0a74b192fb1146880158a584ee14c8e7c9e3396
997962520 <997962520@qq.com>
2016-07-05 16:36:36 +0800

    11

--- Project/teacher/teacher.php
+++ Project/teacher/teacher.php
@@ -91,15 +91,15 @@
                     <?php
                     require_once '../database.php';
                     session_start();
-                    $user_id = $_SESSION["user_id"];                    
+                    $user_id = $_SESSION["user_id"];
                     $my_db = new database();
                     $class_data=$my_db->database_get("select * from class where id=select class_id from class_teacher where teacher_id=$user_id");
                     $teacher_data=$my_db->database_get("select * from teacher where id=$user_id");
                     $count=count($class_data);
                     $i=0;
+                    $html=<<<html
 
-                    echo '
-                    <table class="table">
+                        <table class="table">
                         <thead>
                         <tr>
                             <th>课程编号</th>
@@ -109,64 +109,50 @@
                         </tr>
                         </thead>
                         <tbody>'
-
+html;
+echo $html;
                         $html_01=<<<html
 <tr>
-html;
-                        $html_02=<<<html
 <th scope="row">
 html;
-                        $html_03=<<<html
+                        $html_02=<<<html
 </th>
-html;
-                        $html_04=<<<html
 <td>
-html;
-                        $html_05=<<<html
 <form action="teacher-class-message.php" method="get" name="class_name"><input type="hidden" id="number1"><button type="submit" class="btn-inverse btn" ><p id="classnumber1">
 html;
-                        $html_06=<<<html
+                        $html_03=<<<html
                         </p></button></form></td>
                             <script type="text/javascript">
                                 document.getElementById('number1').value=document.getElementById('classnumber1').value;
                             </script>
+                            <td>
 html;
-                        $html_07=<<<html
-<td>
-html;
-                        $html_08=<<<html
+                        $html_04=<<<html
 </td>
-html;
-                        $html_09=<<<html
 </tr>
 html;
 //开始执行
                         for($i=0;$i<$count;$i++)
                         {
                             echo $html_01;
-                            echo $html_02;
-                            echo"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+                            echo 'asdwadsadwadsa';
                             echo $class_data[$i]['id'];
-                            echo $html_03;
-                            echo $html_04;
-                            echo $html_05;
+                            echo $html_02;
                             echo $class_data[$i]['name'];
-                            echo $html_06;
-                            echo $html_07;
+                            echo $html_03;
                             echo $class_data[$i]['time'];
-                            echo $html_08;
-                            echo $html_09;
+                            echo $html_04;
                         }
                         ?>
                         <!-----
                         <tr>
-                            <th scope="row"><?php echo $class_data[$i]['id']; ?></th>
-                            <td><form action="teacher-class-message.php" method="get" name="class_name"><input type="hidden" id="number1"><button type="submit" class="btn-inverse btn" ><p id="classnumber1"><?php echo $class_data[$i]['name']; ?></p></button></form></td>
+                            <th scope="row"></th>
+                            <td><form action="teacher-class-message.php" method="get" name="class_name"><input type="hidden" id="number1"><button type="submit" class="btn-inverse btn" ><p id="classnumber1"></p></button></form></td>
                             <script type="text/javascript">
                                 document.getElementById('number1').value=document.getElementById('classnumber1').value;
                             </script>
 
-                            <td><?php echo $class_data[$i]['time'];$i++;?></td>
+                            <td></td>
                         </tr>
 
 z
