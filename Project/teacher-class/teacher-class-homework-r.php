342f9446dd3c89d70521223841a593c0ce82b07b
王琦 <740831268@qq.com>
2016-07-07 13:55:36 +0800

    修改作业

--- Project/teacher-class/teacher-class-homework-r.php
+++ Project/teacher-class/teacher-class-homework-r.php
@@ -16,21 +16,19 @@ if(isset($_SESSION['user_id'])) {
     $end_time = $_GET['end_time'];
     $start_time = "2016-7-5 16:11:20";
     $db = new database();
-
     $class_id = $_SESSION['class_id'];
     //$teacher_id = $_SESSION['user_id'];
     $id = $_GET['id'];
-
+    echo $id;
     //判断是否为团队作业
     if ($kind == '团队作业') {
 //        $result = $db->database_get("SELECT * FROM work WHERE class_id=$class_id AND id=$id");
 //        $count = count($_result);
         if ($id!=null) {
-            $db->database_do("UPDATE work set kind=2,title=$title,content=$content,class_id=$class_id,
-                                          start_time=$start_time,end_time=$end_time WHERE id=$id");
 
-            echo "<script type='text/javascript'>alert(\"$sql\");location='teacher-class-givehomework.php';</script>";
-
+            //$sql = "UPDATE work set kind=2,title=$title,content=$content,end_time=$end_time WHERE id=$id";
+            $db->database_do("UPDATE work set kind=2,title='$title',content='$content',end_time='$end_time' WHERE id=$id");
+            echo "<script type='text/javascript'>alert('作业修改成功！');location='teacher-class-givehomework.php';</script>";
         }
         else{
             $values = array('kind' => 2, 'title' => $title, 'content' => $content, 'class_id' => $class_id,
@@ -38,10 +36,7 @@ if(isset($_SESSION['user_id'])) {
             $db->insert_to_db('work', $values);
             echo "<script type='text/javascript'>alert('作业发布成功！');location='teacher-class-givehomework.php';</script>";
         }
-
-
-        }
-
+    }
     //判断是否为个人作业
     elseif ($kind == '个人作业') {
 //        $result1 = $db->database_get("SELECT * FROM work WHERE class_id=$class_id AND id=$id");
@@ -49,9 +44,7 @@ if(isset($_SESSION['user_id'])) {
         if ($id!=null) {
             $db->database_do("UPDATE work set kind=1,title='$title',content='$content',class_id=$class_id,
                                             start_time=$start_time,end_time=$end_time WHERE id=$id");
-
             echo "<script type='text/javascript'>alert('作业修改成功');location='teacher-class-givehomework.php';</script>";
-
         }
         else{
             $values = array('kind' => 1, 'title' => $title, 'content' => $content, 'class_id' => $class_id,
@@ -59,12 +52,8 @@ if(isset($_SESSION['user_id'])) {
             $db->insert_to_db('work', $values);
             echo "<script type='text/javascript'>alert('作业发布成功！');location='teacher-class-givehomework.php';</script>";
         }
-
-
-
     }
-    
-    
+
 
 }
 else {
