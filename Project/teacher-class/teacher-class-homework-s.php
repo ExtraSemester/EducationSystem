cc831258df1a6124fbd4eb11b16f6451b7245b4e
shtb <13240333607@163.com>
2016-07-08 15:18:51 +0800

    删除外链

--- Project/teacher-class/teacher-class-homework-s.php
+++ Project/teacher-class/teacher-class-homework-s.php
@@ -22,9 +22,6 @@ $html_partA = <<<HTML
 <link href="../css/font-awesome.css" rel="stylesheet"> 
 <!-- jQuery -->
 <script src="../js/jquery.min.js"></script>
-<!----webfonts--->
-<link href='http://fonts.useso.com/css?family=Roboto:400,100,300,500,700,900' rel='stylesheet' type='text/css'>
-<!---//webfonts--->  
 <!-- Bootstrap Core JavaScript -->
 <script src="../js/bootstrap.min.js"></script>
 </head>
