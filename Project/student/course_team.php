96ac03786623ad12013ff6703d83bf1872268d66
997962520 <997962520@qq.com>
2016-07-08 11:06:50 +0800

    修改

--- Project/student/course_team.php
+++ Project/student/course_team.php
@@ -251,7 +251,6 @@ require_once '../database.php';
 echo $html_A;
 $my_db=new database();
 $user_id = $_SESSION['user_id'];
-$class_id=$_SESSION['class_id'];
 $user_team_data=$my_db->database_get("select * from team where id in(select team_id from team_student where student_id=$user_id) ");
 $lead_data=$my_db->database_get("select * from student where id=$user_team_data[0]['admin_id']");
 $count=count($user_team_data);
