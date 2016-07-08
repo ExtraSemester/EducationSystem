aa699de5522ee56988a800b82f659dd45faad7a4
HongXingW <sun_whx@163.com>
2016-07-08 12:42:15 +0800

    作业成绩页修改

--- Project/student/s_homework_grade.php
+++ Project/student/s_homework_grade.php
@@ -202,8 +202,9 @@ echo $html_A;
 $my_db=new database();
 $user_id = $_SESSION['user_id'];
 
-$student_work_data=$my_db->database_get("select * from work_file where student_id=$user_id");
+$student_work_data=$my_db->database_get("select work_id,comment,grade from work_file where student_id=$user_id");
 $count_student_work=count($student_work_data);
+
 if($count_student_work==0)
 {
     echo "<td>暂无作业信息</td>";
@@ -212,10 +213,13 @@ else
 {
     for($i=0;$i<$count_student_work;$i++)
     {
+        $work_id = $student_work_data[$i]['work_id'];
+        $work_data = $my_db->database_get("select title from work where id=$work_id");
+
         echo $html_1;
-        echo $student_work_data[$i]['id'];
+        echo $student_work_data[$i]['work_id'];
         echo $html_2;
-        echo $student_work_data[$i]['title'];
+        echo $work_data[0]['title'];
         echo $html_3;
         echo $student_work_data[$i]['grade'];
         echo $html_4;
