80a7ecc0caf35554088f33f88b8abed2d509a7f5
997962520 <997962520@qq.com>
2016-07-10 09:56:06 +0800

    重写

--- Project/teacher-class/teacher-class-message.php
+++ Project/teacher-class/teacher-class-message.php
@@ -1,21 +1,26 @@
+<?php
 
+$html_A=<<<HTML
 <!DOCTYPE HTML>
 <html>
 <head>
-    <title>课程信息</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>课程信息</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 
-    <script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
-    <!-- Bootstrap Core CSS -->
-    <link href="../css/bootstrap.min.css" rel='stylesheet' type='text/css' />
-    <!-- Custom CSS -->
-    <link href="../css/style.css" rel='stylesheet' type='text/css' />
-    <link href="../css/font-awesome.css" rel="stylesheet">
-    <!-- jQuery -->
-    <script src="../js/jquery.min.js"></script>
-    <!-- Bootstrap Core JavaScript -->
-    <script src="../js/bootstrap.min.js"></script>
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+ <!-- Bootstrap Core CSS -->
+<link href="../css/bootstrap.min.css" rel='stylesheet' type='text/css' />
+<!-- Custom CSS -->
+<link href="../css/style.css" rel='stylesheet' type='text/css' />
+<link href="../css/font-awesome.css" rel="stylesheet"> 
+<!-- jQuery -->
+<script src="../js/jquery.min.js"></script>
+<!----webfonts--->
+<link href='' rel='stylesheet' type='text/css'>
+<!---//webfonts--->  
+<!-- Bootstrap Core JavaScript -->
+<script src="../js/bootstrap.min.js"></script>
 </head>
 
 
@@ -24,45 +29,45 @@
 <body>
 
 
-<!------------ 顶边栏 ------------->
-<div id="wrapper">
-    <nav class="top1 navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
-           <p class="navbar-brand" href="" style="font-family:'华文行楷'">北航协同教学平台</p>
-        </div>
-        <!-- /.navbar-header -->
-        <ul class="nav navbar-nav navbar-right">
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-comments-o"></i><span class="badge"></span></a>
-                <ul class="dropdown-menu">
-                    <li class="dropdown-menu-header">
-                        <strong>Messages</strong>
-
-                    </li>
-                    <li class="avatar">
-                        <a href="#">
-                            <img src="../images/1.png" alt=""/>
-                            <div>New message</div>
-                            <small>1 minute ago</small>
-                            <span class="label label-info">NEW</span>
-                        </a>
-                    </li>
-                </ul>
-            </li>
-            <li class="dropdown">
-                <a href="#" class="dropdown-toggle avatar" data-toggle="dropdown"><img src="../images/1.png"></a>
-                <ul class="dropdown-menu">
-                    <li class="m_2"><a href="../teacher/teacher.php"><i class="fa fa-lock"></i> 个人资料</a></li>
-                    <li class="m_2"><a href="#"><i class="fa fa-lock"></i> 设置</a></li>
-                    <li class="m_2"><a href="#"><i class="fa fa-lock" onclick="logout()"></i> 退出</a></li>
-                </ul>
-                <script>
+ <!------------ 顶边栏 ------------->
+ <div id="wrapper">
+        <nav class="top1 navbar navbar-default navbar-static-top" role="navigation" style="margin-bottom: 0">
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+               <p class="navbar-brand" href="" style="font-family:'华文行楷'">北航协同教学平台</p>
+            </div>
+            <!-- /.navbar-header -->
+            <ul class="nav navbar-nav navbar-right">
+				<li class="dropdown">
+	        		<a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-comments-o"></i><span class="badge">4</span></a>
+	        		<ul class="dropdown-menu">
+						<li class="dropdown-menu-header">
+							<strong>Messages</strong>
+							
+						</li>
+						<li class="avatar">
+							<a href="#">
+								<img src="../images/1.png" alt=""/>
+								<div>New message</div>
+								<small>1 minute ago</small>
+								<span class="label label-info">NEW</span>
+							</a>
+						</li>
+	        		</ul>
+	      		</li>
+			    <li class="dropdown">
+	        		<a href="#" class="dropdown-toggle avatar" data-toggle="dropdown"><img src="../images/1.png"></a>
+	        		<ul class="dropdown-menu">
+						<li class="m_2"><a href="#"><i class="fa fa-lock"></i> 个人资料</a></li>	
+                        <li class="m_2"><a href="#"><i class="fa fa-lock"></i> 设置</a></li>	
+                        <li class="m_2"><a href="#" onclick="logout()"><i class="fa fa-lock"></i> 退出</a></li>
+	        		</ul>
+                    <script>
 						function logout(){
 							if (confirm("确认退出？")){
 							   top.location = "../utils/logout.php";
@@ -70,151 +75,120 @@
 						  return false;
 						}
 					</script>
-            </li>
-        </ul>
-        <!------------顶边栏----------------->
-
-        <!------------侧边栏----------------->
-        <div class="navbar-default sidebar" role="navigation">
-
-            <div class="sidebar-nav navbar-collapse">
-                <ul class="nav" id="side-menu">
-                    <li>
-                        <a href="teacher-class-message.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>课程信息</a>
-                    </li>
-                    <li>
-                        <a href="teacher-class-team.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>团队申请</a>
-                    </li>
-                    <li>
-                        <a href="teacher-class-givehomework.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>发布作业</a>
-                    </li>
-                    <li>
-                        <a href="teacher-class-file.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>发布资源</a>
-                    </li>
-                    <li>
-                        <a href="teacher-class-homework.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>已交作业</a>
-                    </li>
-					<li>
-						<a href="talk.php"><i class="fa fa-comments nav_icon"></i>课程讨论</a>
-					</li>
-                </ul>
+	      		</li>
+			</ul>
+<!------------顶边栏----------------->
+            
+<!------------侧边栏-----------------> 
+			<div class="navbar-default sidebar" role="navigation">
+            
+                <div class="sidebar-nav navbar-collapse">
+                    <ul class="nav" id="side-menu">
+                        <li>
+                            <a href="teacher-class-message.html"><i class="fa fa-dashboard fa-fw nav_icon"></i>课程信息</a>
+                        </li>
+                           <li>
+                            <a href="teacher-class-team.html"><i class="fa fa-dashboard fa-fw nav_icon"></i>团队申请</a>
+                        </li>
+                           <li>
+                            <a href="teacher-class-givehomework.html"><i class="fa fa-dashboard fa-fw nav_icon"></i>发布作业</a>
+                        </li>
+                        <li>
+                            <a href="teacher-class-file.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>发布资源</a>
+                        </li>
+                        <li>
+                            <a href="teacher-class-homework.php"><i class="fa fa-dashboard fa-fw nav_icon"></i>已交作业</a>
+                        </li>
+                            </ul>
+                </div>
             </div>
-        </div>
-    </nav>
-    <!------------侧边栏----------------->
-
-
-    <!---------课程信息表格----------->
-    <div id="page-wrapper">
-        <div class="tablegraphs">
-            <div class="col-md-12 graphs">
-                <div class="xs">
-                    <h3>课程信息</h3>
-                    <div class="bs-example4" data-example-id="contextual-table">
-                        <?php
-                        require_once '../database.php';
-                        session_start();
-                        $my_db=new database();
-                        $class_id=$_GET['class_id'];
-                        if($class_id == null){
-                            $class_id = $_SESSION['class_id'];
-                        }else{
-                            $_SESSION['class_id'] = $class_id;
-                        }
-
-                        $class_data=$my_db->database_get("select * from class where id='$class_id' ");
-                        $class_teacher_data=$my_db->database_get("select * from teacher where id in (select teacher_id from class_teacher where class_id=$class_id)");
-
-                        ?>
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th>课程代号</th>
-                                <th>课程名称</th>
-                                <th>任课教师</th>
-                                <th>课程状态</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                            <tr class="active">
-                                <th scope="row"><?php echo $class_data[0]['id'] ?></th>
-                                <td><?php echo $class_data[0]['name']?></td>
-                                <td><?php echo $class_teacher_data[0]['name']?></td>
-                                <td><?php echo $class_data[0]['state']?></td>
-                            </tr>
-                            </tbody>
-                        </table>
-                    </div>
-                    <?php
-                    require_once '../database.php';
-
-                    $my_db=new database();
-                    $class_data_id=$class_data[0]['id'];
-                    $team_data=$my_db->database_get("select * from team where class_id=$class_data_id");
-                    $count=count($team_data);
-                    $i=0;
-                    $html= <<<HTML
-                    <tr>
-                    <div class="panel-body1">
-                        <table class="table">
-                            <thead>
-                            <tr>
-                                <th>团队编号</th>
-                                <th>团队名称</th>
-                                <th>团队人数</th>
-                                <th>团队负责人</th>
-                            </tr>
-                            </thead>
-                            
-                        </table>
-                    </div>
-                </div><!-- /.table-responsive -->
-            </div>
-        </div>
-                     </tr>
-        
+        </nav>
+<!------------侧边栏----------------->   
+      
+      
+<!---------课程信息表格----------->
+<div id="page-wrapper">
+<div class="tablegraphs">
+   <div class="col-md-12 graphs">
+	   <div class="xs">
+  	 <h3>课程信息</h3>
+  	<div class="bs-example4" data-example-id="contextual-table">
+    <table class="table">
+      <thead>
+        <tr>
+          <th>课程代号</th>
+          <th>课程名称</th>
+          <th>任课教师</th>
+          <th>课程状态</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr class="active">
+          <th scope="row">
 HTML;
-echo $html;
-                    $html_1=<<<HTML
-                            <tbody>
+$html_01=<<<HTML
+</th>
+          <td>
 HTML;
-                    $html_2=<<<HTML
-                            <tr>
+$html_02=<<<HTML
+</td>
+          <td>
 HTML;
-                    $html_3=<<<HTML
-                            <td>
+$html_03=<<<HTML
+</td>
+          <td>
 HTML;
-                    $html_4=<<<HTML
-                            </td>
-HTML;
-                    $html_5=<<<HTML
-                            </tr>
-HTML;
-                    $html_6=<<<HTML
-                            </tbody>
-HTML;
-                    echo $html_1;
-                    echo $html_2;
-for($i=0;$i<$count;$i++)
-{
-    echo $html_3,$team_data[$i]['id'],$html_4,$html_3,$team_data[$i]['name'],$html_4,$html_3,$team_data[$i]['number'],$html_4,$html_3,$team_data[$i]['admin_id'],$html_4;
-}
-                    echo $html_5;
-                    echo $html_6;
-                    
-                    ?>
 
-        <!------------课程信息表格------------->
+$html_B=<<<HTML
+        <tr>
+          <th>团队编号</th>
+          <th>团队名称</th>
+          <th>团队人数</th>
+          <th>团队负责人</th>
+        </tr>
+      </thead>
+      <tbody>
+HTML;
 
+$html_1=<<<HTML
+        <tr>
+          <th>
+HTML;
+$html_2=<<<HTML
+</th>
+          <td>
+HTML;
+$html_3=<<<HTML
+</td>
+          <td>
+HTML;
+$html_4=<<<HTML
+</td>
+          <td
+HTML;
+$html_5=<<<HTML
+</td>
+        </tr>
+HTML;
 
-        <!-------------边底栏信息-------------->
-        <div class="copy_layout">
-            <p>BUAA<a href="">协同教学平台.&nbsp;</a> Copyright &copy; 2016.沉迷学习</p>
-        </div>
+$html_C=<<<HTML
+      </tbody>
+    </table>
     </div>
-</div>
-<!-- /#page-wrapper -->
-</div>
+    </div><!-- /.table-responsive -->
+  </div>
+  </div>
+<!------------课程信息表格------------->
+
+
+<!-------------边底栏信息-------------->
+  <div class="copy_layout">
+      <p>Copyright &copy; 2015.Company name All rights reserved.More Templates <a href="http://www.cssmoban.com/" target="_blank" title="模板之家">模板之家</a> - Collect from <a href="http://www.cssmoban.com/" title="网页模板" target="_blank">网页模板</a></p>
+  </div>
+   </div>
+      </div>
+      <!-- /#page-wrapper -->
+   </div>
 <!-------------边底栏信息-------------->
 
 
@@ -229,3 +203,49 @@ for($i=0;$i<$count;$i++)
 <script src="../js/custom.js"></script>
 </body>
 </html>
+HTML;
+
+require_once '../database.php';
+
+echo $html_A;
+session_start();
+$my_db=new database();
+$class_id=$_GET['class_id'];
+if($class_id == null){
+    $class_id = $_SESSION['class_id'];
+}else{
+    $_SESSION['class_id'] = $class_id;
+}
+
+$class_data=$my_db->database_get("select * from class where id='$class_id' ");
+$class_teacher_data=$my_db->database_get("select * from teacher where id in (select teacher_id from class_teacher where class_id=$class_id)");
+
+//课程信息
+echo $class_data[0]['id'];
+echo $html_01;
+echo $class_data[0]['name'];
+echo $html_02;
+echo $class_teacher_data[0]['name'];
+echo $html_03;
+echo $class_data[0]['state'];
+echo $html_B;
+
+//团队信息
+$class_data_id=$class_data[0]['id'];
+$team_data=$my_db->database_get("select * from team where class_id=$class_data_id");
+$team_admin_id=$team_data[0]['admin_id'];
+$team_lead_name=$my_db->database_get("select name from student where id=$team_admin_id");
+$count=count($team_data);
+for($i=0;$i<$count;$i++)
+{
+    echo $html_1;
+    echo $team_data[$i]['id'];
+    echo $html_2;
+    echo $team_data[$i]['name'];
+    echo $html_3;
+    echo $team_data[$i]['number'];
+    echo $html_4;
+    echo $team_lead_name[$i]['name'];
+    echo $html_5;
+}
+echo $html_C;
